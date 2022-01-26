35070615fc36810bc1a18c2774d7c1331a60087d
Salvatore949 <salvo.mess49@gmail.com>
2022-01-26 09:43:47 +0100

    table made

---
+++ database/migrations/2022_01_26_001015_create_singers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSingersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('singers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('album_id') -> unsigned();
+            $table->string('artist');
+            $table->string('genre');
+            $table->YEAR('year');
+            $table->string('songs');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('singers');
+    }
+}
